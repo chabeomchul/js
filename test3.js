28e49afd9699eae5fcfd43a463c84f3e781e117e
Cha Beom Chul <c7h4a@naver.com>
2018-10-18 16:44:28 +0900

    5st commit

--- test3.js
+++ test3.js
@@ -1,2 +1,18 @@
 console.log('hi! chabeom chul');
 console.log('hi! chabeom chul33');
+
+function setName(obj) {
+    obj.name = 'chabeomchul';
+    obj = new Object();
+    obj.name = 'change name';
+}
+
+var person = new Object();
+setName(person);
+console.log(person.name);
+
+
+// array test
+
+var arrTest = ['asdf','xdfd','ewes','dxws','xxxw','ffff'];
+arrTest.forEach((x, i) => console.log(i + "." + x));
